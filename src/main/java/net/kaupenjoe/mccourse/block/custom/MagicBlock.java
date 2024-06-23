aad090dfdffc500681117eb9d8e9ed7caa492d67
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-06-23 19:21:24 +0200

    custom tags

--- src/main/java/net/kaupenjoe/mccourse/block/custom/MagicBlock.java
+++ src/main/java/net/kaupenjoe/mccourse/block/custom/MagicBlock.java
@@ -1,6 +1,7 @@
 package net.kaupenjoe.mccourse.block.custom;
 
 import net.kaupenjoe.mccourse.item.ModItems;
+import net.kaupenjoe.mccourse.util.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
@@ -44,8 +45,7 @@ public class MagicBlock extends Block {
     }
 
     private boolean isValidItem(ItemStack item) {
-        return item.getItem() == ModItems.RAW_BLACK_OPAL.get()
-                || item.getItem() == Items.COAL || item.getItem() == Items.DANDELION;
+        return item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
---
+++ src/main/java/net/kaupenjoe/mccourse/util/ModTags.java
@@ -0,0 +1,25 @@
+package net.kaupenjoe.mccourse.util;
+
+import net.kaupenjoe.mccourse.MCCourseMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+        private static TagKey<Block> createTag(String name) {
+            return BlockTags.create(ResourceLocation.fromNamespaceAndPath(MCCourseMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        public static final TagKey<Item> TRANSFORMABLE_ITEMS = createTag("transformable_items");
+
+        private static TagKey<Item> createTag(String name) {
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(MCCourseMod.MOD_ID, name));
+        }
+    }
+}
